e24c4ba7d35a31998e79c6585809a9b31fe799ed
Kingy-1 <74762680+Kingy-1@users.noreply.github.com>
2026-07-27 21:06:36 +0800

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.py
@@ -0,0 +1,32 @@
+from collections import defaultdict
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        ans = []
+        temp = defaultdict()
+
+        for i, num in enumerate(nums):
+            temp[num] = i
+
+        print(temp) 
+        
+        # temp is nums
+        # for each letter in nums check if 7-that letter is in nums
+        # if so add the index of that value
+        # 
+        lookingFor = None
+        found = False
+
+        for index, value in enumerate(nums):
+            if lookingFor == value:
+                print(value)
+                ans.append(index)
+                
+            if target-value in nums and found == False and temp[target-value] != index:
+                ans.append(index)
+                found = True
+                print(value)
+                lookingFor = target-value
+        
+        
+        return [ans[0], ans[1]]
